4e6f263d970a476c2673c76ba6a9fea4c19f54db
André Pacheco Neves <andre.neves@mindera.com>
2018-04-07 02:45:06 +0100

    Add project

---
+++ CPTestCustomModuleMapAndPrivateHeader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CPTestCustomModuleMapAndPrivateHeader
+//
+//  Created by André Pacheco Neves on 07/04/2018.
+//  Copyright © 2018 p4checo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+inhibit_all_warnings!
+
+target 'CPTestCustomModuleMapAndPrivateHeader' do
+
+  pod 'ReactiveCocoa', '~> 7.0', :modular_headers => true
+
+end
